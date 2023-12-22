80a664215b7659e90b52fbe64d6a5da61bd57c06
B00150149 <B00150149@mytudublin.ie>
2023-12-22 13:33:27 +0530

    Button

---
+++ Challange4/Assets/Challenge 4/Scripts/Link.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+using UnityEngine.UI;
+
+public class Link : MonoBehaviour 
+{
+
+    public void OpenGithub()
+    {
+        Application.OpenURL("https://github.com/B00150149/CHALLANGE4M");
+    }
+
+
+ 
+
+}
